0e4d77b53778e9565f7f1dc79832a1fbe1659bcd
Juandi <joa102@inlumine.ual.es>
2022-04-08 09:12:31 +0200

    Cambios en los metodos de rutas en UserController

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -6,12 +6,12 @@ import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.PutMapping;
+// import org.springframework.web.bind.annotation.DeleteMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -22,13 +22,14 @@ public class UserController {
 	@Autowired
 	private UserRepository repository;
 	
-	
+	// Ruta personalizada para obtener todos los usuarios
 	@GetMapping("/users")
 	public List<User> allUsers(){
 		return (List<User>) repository.findAll();
 	}
 	
-	@GetMapping("/user/{name}")
+	// Ruta personalizada para buscar por nombre
+	@GetMapping("/user/name/{name}")
 	public List<User> findByName(@PathVariable("name") String name) {
 		return repository.findByName(name);
 	}
@@ -39,18 +40,18 @@ public class UserController {
 		return repository.findBySurname(surname);
 	}
 	
-	@PostMapping("/user")
-	public User createUser(@RequestBody User user) {
-		return repository.save(user);
-	}
+	// @PostMapping("/user")
+	// public User createUser(@RequestBody User user) {
+	// 	return repository.save(user);
+	// }
 	
-	@PutMapping("/user/{id}")
-	public User updateUser(@PathVariable int id ,@RequestBody User user) {
-		return repository.save(user);
-	}
+	// @PutMapping("/user/{id}")
+	// public User updateUser(@PathVariable int id ,@RequestBody User user) {
+	// 	return repository.save(user);
+	// }
 	
-	@DeleteMapping("/user/{id}")
-	public void deleteUser(@PathVariable("id") Long id) {
-		repository.deleteById(id);
-	}
+	// @DeleteMapping("/user/{id}")
+	// public void deleteUser(@PathVariable("id") Long id) {
+	// 	repository.deleteById(id);
+	// }
 }
